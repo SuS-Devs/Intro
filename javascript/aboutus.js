29f1aa421b01c431d997ddf178a4c2a213beb6e8
thehero12347 <108070538+SuS-Devs@users.noreply.github.com>
2022-06-24 14:53:09 +0000

    github button working now there are still some padding prob but it will be fixed, on click js function implemented some working some not

---
+++ javascript/aboutus.js
@@ -0,0 +1,7 @@
+document.getElementById("logo").onclick = function (logo) {
+  location.href="/index.html";
+};
+
+document.getElementById("github").onclick = function (git) {
+  location.href="https://github.com/thehero12347";
+};
---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+document.getElementById("logo").onclick = function (logo) {
+  location.href="index.html";
+};
+
+document.getElementById("github").onclick = function (git) {
+  location.href="https://github.com/thehero12347";
+};
+
